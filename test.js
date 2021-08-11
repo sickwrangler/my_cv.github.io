2214c67ab724420c88d6dcd3889367b22d89ccaa
sickwrangler <patrickj.jarvis@gmail.com>
2021-08-11 17:57:18 +0100

    commit main

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-$(document).ready(function () {
-    debugger;
-      $("section.workarea-view-content >div").attr({
-        role: "main",
-        "data-skip-target": "main"
-      });
-  });
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
