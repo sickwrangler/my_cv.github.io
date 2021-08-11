4588adfa0f1d5f1a2275ad1bc33da811020b30d5
sickwrangler <patrickj.jarvis@gmail.com>
2021-08-11 17:47:52 +0100

    commit on main

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-$(function () {
-    var title = document.title;
-    var ind = title.indexOf("NCD");
-    if (ind > -1) {
-        document.title = document.title.substr(document.title.indexOf("-") + 1);
-    }
-});
+$(document).ready(function () {
+    debugger;
+      $("section.workarea-view-content >div").attr({
+        role: "main",
+        "data-skip-target": "main"
+      });
+  });
+
