4620ed650ddf0fe7eeaea1713c5f9bb24eb4ee19
sickwrangler <patrickj.jarvis@gmail.com>
2021-09-16 17:24:39 +0100

    Googley eyes working

--- test.js
+++ test.js
@@ -1,18 +1,40 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-  var coll = document.getElementsByClassName("collapsible");
-var i;
+/*****************Collapsible Buttons JS ******************/
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
+document.addEventListener('DOMContentLoaded', function () {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 }, false);
 
+
+
+/*****************************Moving eyes JS ************************/
+//This is a pen based off of Codewoofy's eyes follow mouse. It is just cleaned up, face removed, and then made to be a little more cartoony. https://codepen.io/Codewoofy/pen/VeBJEP
+$(document).ready(function () {
+
+  $(".container").mousemove(function (event) {
+    var eye = $(".eye");
+    var x = (eye.offset().left) + (eye.width() / 2);
+    var y = (eye.offset().top) + (eye.height() / 2);
+    var rad = Math.atan2(event.pageX - x, event.pageY - y);
+    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    eye.css({
+      '-webkit-transform': 'rotate(' + rot + 'deg)',
+      '-moz-transform': 'rotate(' + rot + 'deg)',
+      '-ms-transform': 'rotate(' + rot + 'deg)',
+      'transform': 'rotate(' + rot + 'deg)'
+    });
+  });
+});
