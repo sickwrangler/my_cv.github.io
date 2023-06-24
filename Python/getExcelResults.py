7db31ae6197ba264d76ba8139f8e629968ad46d2
Paddy Jarvis <48927793+sickwrangler@users.noreply.github.com>
2023-06-24 17:41:41 +0000

    commit

--- Python/getExcelResults.py
+++ Python/getExcelResults.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
-file_path = "/workspaces/my_cv.github.io/ParcBrynBachRunningClub.xlsx"
-df = pd.read_excel(file_path)
-
+file_path = "/workspaces/my_cv.github.io/ParcBrynBachResults - Results (All Time).csv"
+df = pd.read_csv(file_path)
   
-print(df)  
+personal_bests = df.query("Best == 'PB'")
+latest_pbs = personal_bests.head(25)
+#print(latest_pbs)
+
+season_bests = df.query("Best == 'SB'")
+latest_sbs = season_bests.head(25)
+#print(latest_sbs)
+
+club_record = df.query("`Club Record` == 'Y'")
+
+print(club_record)
+
+# first way
+#sorted = club_record.sort_values(['Date'], ascending = [True])
+
+#first = sorted.groupby('Distance').first().reset_index()
+
+#print(sorted)
