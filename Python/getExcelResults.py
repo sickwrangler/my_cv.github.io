c292c5000fb6b369b3896b8b616dbbe254b98fea
Paddy Jarvis <48927793+sickwrangler@users.noreply.github.com>
2023-07-02 11:11:45 +0000

    one commit

--- Python/getExcelResults.py
+++ Python/getExcelResults.py
@@ -1,23 +1,27 @@
 import pandas as pd
 
+#get CSV data and turn this into a dataframe
 file_path = "/workspaces/my_cv.github.io/ParcBrynBachResults - Results (All Time).csv"
 df = pd.read_csv(file_path)
   
+#Get the 25 recent PBs
 personal_bests = df.query("Best == 'PB'")
 latest_pbs = personal_bests.head(25)
 #print(latest_pbs)
 
+#Get the 25 recent SBs
 season_bests = df.query("Best == 'SB'")
 latest_sbs = season_bests.head(25)
 #print(latest_sbs)
 
-club_record = df.query("`Club Record` == 'Y' & Sex == 'W' & `Age Category` == 'SEN'")
-
+#Get current club records
+#club_record = df.query("`Club Record` == 'Y' & Sex == 'W' & `Age Category` == 'SEN'")
 #print(club_record)
-
 # first way
-sorted = club_record.sort_values(['Date'], ascending = [True])
+#sorted = club_record.sort_values(['Date'], ascending = [True])
+#first = sorted.groupby('Distance').first().reset_index()
+#print(first)
 
-first = sorted.groupby('Distance').first().reset_index()
+pb_html_table = latest_pbs.to_html()
 
-print(first)
+print(pb_html_table)
