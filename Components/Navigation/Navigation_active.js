70b6270df231f086bb30dc419d9810e520559e21
Paddy Jarvis <48927793+sickwrangler@users.noreply.github.com>
2023-07-02 12:49:58 +0000

    i want it all

---
+++ Components/Navigation/Navigation_active.js
@@ -0,0 +1,22 @@
+// Get the current page URL
+var currentURL = window.location.href;
+
+// Get the path part of the URL (excluding the domain)
+var path = window.location.pathname;
+
+// Get all the navigation links
+var links = document.querySelectorAll('nav a');
+
+// Iterate through each link and check if it matches the current page
+for (var i = 0; i < links.length; i++) {
+  var link = links[i];
+
+  // Get the href attribute value of the link
+  var linkHref = link.getAttribute('href');
+
+  // Check if the current page URL contains the link's href value
+  if (currentURL.includes(linkHref)) {
+    link.classList.add('active'); // Add the "active" class
+    break; // Stop iterating once a match is found
+  }
+}
--- Components/Navigation/navigation.js
+++ Components/Navigation/navigation.js
@@ -1,25 +1,63 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+  
+  connectedCallback() {
+    this.innerHTML = `
+    <style>
+    /* Add a black background color to the top navigation */
+    .topnav {
+      background-color: lightgrey;
+      overflow: hidden;
+      z-index: 2;
+      position: sticky;
+    }
+    
+    /* Style the links inside the navigation bar */
+    .topnav a {
+      float: left;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+      color: black;
+    }
+    
+    /* Change the color of links on hover */
+    .topnav a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+    
+    /* Add a color to the active/current link */
+    .topnav a.active {
+      background-color: royalblue;
+      color: white;
+    }
+    
+    </style>
 
-    debugger;
-// Get the current page URL
-var currentURL = window.location.href;
-
-// Get the path part of the URL (excluding the domain)
-var path = window.location.pathname;
-
-// Get all the navigation links
-var links = document.querySelectorAll('nav a');
-
-// Iterate through each link and check if it matches the current page
-for (var i = 0; i < links.length; i++) {
-  var link = links[i];
-
-  // Get the href attribute value of the link
-  var linkHref = link.getAttribute('href');
-
-  // Check if the current page URL ends with the link's href value
-  if (currentURL.includes(linkHref)) {
-    link.classList.add('active'); // Add the "active" class
-    break; // Stop iterating once a match is found
-  }
+    <html>
+    <head>
+       
+    <link rel="stylesheet" type="text/css" href="top_nav_styles.css" />
+    </head>
+    <div class="topnav">
+        <nav>
+        <a href="/index.html">Home</a>
+        <a href="/Rhedeg/rhedeg.HTML">Rhedeg</a>
+        <a href="/When/WhenShallIRun.HTML">When should you run?</a>
+        <a href="/Map/Map.HTML">Map</a>
+        <a href="/canlyniadaur/ClubActivities.HTML">Club activities</a>
+    </nav>
+      </div>
+     
+    
+    </html>
+    `;
+    }
 }
-
+    customElements.define('header-component', Header);
